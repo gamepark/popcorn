a927f21a8c1bf469d5751a9566dc12775ca9f5a0
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-03-14 22:12:27 +0100

    [5] Feat: added movie cards description (TG-13 #ready-for-test)

--- app/src/material/Material.ts
+++ app/src/material/Material.ts
@@ -1,4 +1,7 @@
 import { MaterialType } from '@gamepark/game-template/material/MaterialType'
 import { MaterialDescription } from '@gamepark/react-game'
+import { movieCardDescription } from './MovieCardDescription'
 
-export const Material: Partial<Record<MaterialType, MaterialDescription>> = {}
+export const Material: Partial<Record<MaterialType, MaterialDescription>> = {
+  [MaterialType.MovieCards]: movieCardDescription
+}
