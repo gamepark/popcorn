708dcfb9e632c1a320eb17483f8fed6a717bf3e4
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-03-16 16:33:18 +0100

    [5] Feat: added Features tile description (TG-16 #ready-for-test)

---
+++ app/src/material/FeaturesTileDescription.ts
@@ -0,0 +1,19 @@
+import { LocationType } from '@gamepark/game-template/material/LocationType'
+import { MaterialType } from '@gamepark/game-template/material/MaterialType'
+import { PlayerColor } from '@gamepark/game-template/PlayerColor'
+import { TokenDescription } from '@gamepark/react-game'
+import featureTile from '../images/Tiles/MovieTiles/FeaturesTile.png'
+
+class FeaturesTileDescription extends TokenDescription<PlayerColor, MaterialType, LocationType> {
+  image = featureTile
+  width = 5
+  height = 7
+
+  staticItem = {
+    location: {
+      type: LocationType.FeaturesTileSpot
+    }
+  }
+}
+
+export const featuresTileDescription = new FeaturesTileDescription()
--- app/src/material/Material.ts
+++ app/src/material/Material.ts
@@ -2,6 +2,7 @@ import { MaterialType } from '@gamepark/game-template/material/MaterialType'
 import { MaterialDescription } from '@gamepark/react-game'
 import { awardCardDescription } from './AwardCardDescription'
 import { bottomCinemaBoardDescription } from './BottomCinemaBoardDescription'
+import { featuresTileDescription } from './FeaturesTileDescription'
 import { lobbySliderDescription } from './LobbySliderDescription'
 import { movieCardDescription } from './MovieCardDescription'
 import { topCinemaBoardDescription } from './TopCinemaBoardDescription'
@@ -11,5 +12,6 @@ export const Material: Partial<Record<MaterialType, MaterialDescription>> = {
   [MaterialType.AwardCards]: awardCardDescription,
   [MaterialType.BottomCinemaBoard]: bottomCinemaBoardDescription,
   [MaterialType.TopCinemaBoard]: topCinemaBoardDescription,
-  [MaterialType.LobbySliders]: lobbySliderDescription
+  [MaterialType.LobbySliders]: lobbySliderDescription,
+  [MaterialType.FeaturesTile]: featuresTileDescription
 }
