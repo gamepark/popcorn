d42c88068247f2335b8d3e8fff777a0356b47801
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-03-17 01:05:57 +0100

    [6] Feat: added premier movie cards row locator (TG-34 #ready-for-test)

--- app/src/locators/Locators.ts
+++ app/src/locators/Locators.ts
@@ -5,11 +5,13 @@ import { Locator } from '@gamepark/react-game'
 import { featureMovieCardsRowLocator } from './FeatureMovieCardsRowLocator'
 import { featuresTilesLocator } from './FeaturesTilesLocator'
 import { movieCardsDeckLocator } from './MovieCardsDeckLocator'
+import { premierMovieCardsRowLocator } from './PremierMovieCardsRowLocator'
 import { premiersTileLocator } from './PremiersTileLocator'
 
 export const Locators: Partial<Record<LocationType, Locator<PlayerColor, MaterialType, LocationType>>> = {
   [LocationType.MovieCardDeckSpot]: movieCardsDeckLocator,
   [LocationType.FeaturesTileSpot]: featuresTilesLocator,
   [LocationType.PremiersTileSpot]: premiersTileLocator,
-  [LocationType.FeaturesRowSpot]: featureMovieCardsRowLocator
+  [LocationType.FeaturesRowSpot]: featureMovieCardsRowLocator,
+  [LocationType.PremiersRowSpot]: premierMovieCardsRowLocator
 }
