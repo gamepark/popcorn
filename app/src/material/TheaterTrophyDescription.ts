db0f716da6ea787a3fe1df67c7bccce4f34ee856
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-03-18 11:35:52 +0100

    [6] Feat: added theater trophies description and reserve locator (TG-39 #ready-for-test, TG-24 #ready-for-test)

---
+++ app/src/material/TheaterTrophyDescription.ts
@@ -0,0 +1,26 @@
+import { LocationType } from '@gamepark/game-template/material/LocationType'
+import { MaterialType } from '@gamepark/game-template/material/MaterialType'
+import { theaterTrophy, TheaterTrophy } from '@gamepark/game-template/material/TheaterTrophy'
+import { PlayerColor } from '@gamepark/game-template/PlayerColor'
+import { ComponentSize, MaterialContext, TokenDescription } from '@gamepark/react-game'
+import { MaterialItem } from '@gamepark/rules-api'
+import trophySecond from '../images/Tokens/3TheatreTrophy.png'
+import trophyFirst from '../images/Tokens/5TheatreTrophy.png'
+
+const trophySizes = {
+  [TheaterTrophy.TrophyFirst]: { width: 3.83, height: 6.94 },
+  [TheaterTrophy.TrophySecond]: { width: 3.28, height: 5.92 }
+}
+
+class TheaterTrophyDescription extends TokenDescription<PlayerColor, MaterialType, LocationType, TheaterTrophy> {
+  public getSize(id: TheaterTrophy): ComponentSize {
+    return trophySizes[id]
+  }
+
+  images = {
+    [TheaterTrophy.TrophyFirst]: trophyFirst,
+    [TheaterTrophy.TrophySecond]: trophySecond
+  }
+}
+
+export const theaterTrophyDescription = new TheaterTrophyDescription()
