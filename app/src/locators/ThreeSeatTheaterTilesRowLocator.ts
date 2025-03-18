dcf3cf252d7e7b1a9143b5202744926e4c9fc9e3
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-03-18 09:39:14 +0100

    [6] Feat: added three seat theater tiles row locator (TG-45 #ready-for-test)

---
+++ app/src/locators/ThreeSeatTheaterTilesRowLocator.ts
@@ -0,0 +1,11 @@
+import { LocationType } from '@gamepark/game-template/material/LocationType'
+import { MaterialType } from '@gamepark/game-template/material/MaterialType'
+import { PlayerColor } from '@gamepark/game-template/PlayerColor'
+import { ListLocator } from '@gamepark/react-game'
+
+class ThreeSeatTheaterTilesRowLocator extends ListLocator<PlayerColor, MaterialType, LocationType> {
+  coordinates = { x: 35, y: -1 }
+  gap = { y: 4.5 }
+}
+
+export const threeSeatTheaterTilesRowLocator = new ThreeSeatTheaterTilesRowLocator()
