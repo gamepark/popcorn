76becbf638b52818f65550d7ba0e5ca368595c8f
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-03-30 10:01:56 +0200

    [TG-87 #in-progress] Feat: added avatar, cover, favicon and background

---
+++ app/src/PopcornTheme.ts
@@ -0,0 +1,10 @@
+import redCarpetBackground from './images/Background.jpg'
+
+export const PopcornTheme = {
+  root: {
+    background: {
+      image: redCarpetBackground,
+      overlay: 'rgba(0, 0, 0, 0.45)'
+    }
+  }
+}
