c1a2defa003834024b83503ac1f6210623b9d7f6
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-03-30 21:40:26 +0200

    [TG-95 #ready-for-test] Feat: added pass button to header

--- app/src/headers/Headers.tsx
+++ app/src/headers/Headers.tsx
@@ -1,7 +1,9 @@
 import { RuleId } from '@gamepark/game-template/rules/RuleId'
 import { ComponentType } from 'react'
+import { BuyingPhaseHeader } from './BuyingPhaseHeader'
 import { DealAndDiscardAwardCardsHeader } from './DealAndDiscardAwardCardsHeader'
 
 export const Headers: Partial<Record<RuleId, ComponentType>> = {
-  [RuleId.DealAndDiscardAwardCards]: DealAndDiscardAwardCardsHeader
+  [RuleId.DealAndDiscardAwardCards]: DealAndDiscardAwardCardsHeader,
+  [RuleId.BuyingPhaseRule]: BuyingPhaseHeader
 }
