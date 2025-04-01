24af67bedb0aae2569176044334cfa5fb492b5b3
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-04-02 01:23:26 +0200

    [TG-92] Refactor: added two seat tiles characteristics

---
+++ rules/src/material/TheaterTiles/TwoSeatGreenBlueCharacteristics.ts
@@ -0,0 +1,23 @@
+import { SeatAction, SeatColor, SeatsNumber } from '../TheaterTile'
+import { getTheaterSeatAction, getTheaterSeatColor, TheaterTileCharacteristics } from './TheaterTileCharacteristics'
+
+export class TwoSeatGreenBlueCharacteristics implements TheaterTileCharacteristics {
+  private readonly seatActions: SeatAction[] = [SeatAction.MovieAction, SeatAction.Get2Popcorn]
+  private readonly seatColors: SeatColor[] = [SeatColor.Green, SeatColor.Blue]
+
+  public getPrice(): number {
+    return 5
+  }
+
+  public getSeatAction(seatNumber: number): SeatAction | undefined {
+    return getTheaterSeatAction(this.seatActions, seatNumber)
+  }
+
+  public getSeatColor(seatNumber: number): SeatColor | undefined {
+    return getTheaterSeatColor(this.seatColors, seatNumber)
+  }
+
+  public getSeatsNumber(): SeatsNumber {
+    return SeatsNumber.Two
+  }
+}
---
+++ rules/src/material/TheaterTiles/TwoSeatYellowReserve2MoneyCharacteristics.ts
@@ -0,0 +1,23 @@
+import { SeatAction, SeatColor, SeatsNumber } from '../TheaterTile'
+import { getTheaterSeatAction, getTheaterSeatColor, TheaterTileCharacteristics } from './TheaterTileCharacteristics'
+
+export class TwoSeatYellowReserve2MoneyCharacteristics implements TheaterTileCharacteristics {
+  private readonly seatColors: SeatColor[] = [SeatColor.Yellow, SeatColor.Grey]
+  private readonly seatActions: SeatAction[] = [SeatAction.PlaceGuestInReserve, SeatAction.Get2Money]
+
+  public getPrice(): number {
+    return 4
+  }
+
+  public getSeatAction(seatNumber: number): SeatAction | undefined {
+    return getTheaterSeatAction(this.seatActions, seatNumber)
+  }
+
+  public getSeatColor(seatNumber: number): SeatColor | undefined {
+    return getTheaterSeatColor(this.seatColors, seatNumber)
+  }
+
+  public getSeatsNumber(): SeatsNumber {
+    return SeatsNumber.Two
+  }
+}
