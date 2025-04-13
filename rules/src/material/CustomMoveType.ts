4103ae71c3bc7526c29cf45372b44f5785768f7c
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-04-13 12:06:14 +0200

    [TG-89 #ready-for-test] Feat: implemented BuyingTheaterTile rule

--- rules/src/material/CustomMoveType.ts
+++ rules/src/material/CustomMoveType.ts
@@ -5,7 +5,8 @@ import { MaterialType } from './MaterialType'
 
 export enum CustomMoveType {
   BuyMovieCard = 1,
-  PassBuyingPhase
+  PassBuyingPhase,
+  BuyTheaterTile
 }
 
 export type BuyMovieCardCustomMoveData = {
@@ -18,6 +19,16 @@ export type BuyMovieCardCustomMove = Omit<CustomMove, 'data'> & {
   data: BuyMovieCardCustomMoveData
 }
 
+export type BuyTheaterTileCustomMoveData = {
+  boughtTileIndex: number
+  player: PlayerColor
+  destinationSpot: 0 | 1 | 2
+}
+
+export type BuyTheaterTileCustomMove = Omit<CustomMove, 'data'> & {
+  data: BuyTheaterTileCustomMoveData
+}
+
 export type PassBuyingPhaseCustomMove = Omit<CustomMove, 'data'>
 
 export const isBuyMovieCardCustomMove = (move: MaterialMove<PlayerColor, MaterialType, LocationType>): move is BuyMovieCardCustomMove =>
@@ -25,3 +36,6 @@ export const isBuyMovieCardCustomMove = (move: MaterialMove<PlayerColor, Materia
 
 export const isPassBuyingPhaseCustomMove = (move: MaterialMove<PlayerColor, MaterialType, LocationType>): move is PassBuyingPhaseCustomMove =>
   isCustomMoveType<CustomMoveType>(CustomMoveType.PassBuyingPhase)(move)
+
+export const isBuyTheaterTileCustomMove = (move: MaterialMove<PlayerColor, MaterialType, LocationType>): move is BuyTheaterTileCustomMove =>
+  isCustomMoveType<CustomMoveType>(CustomMoveType.BuyTheaterTile)(move)
