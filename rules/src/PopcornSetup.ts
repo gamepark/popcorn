35900a385ba2d4de2c745b492154d283c506ece0
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-04-14 21:38:39 +0200

    [TG-97 #in-progress] Fix: fixed some locator coordinates

--- rules/src/PopcornSetup.ts
+++ rules/src/PopcornSetup.ts
@@ -142,7 +142,7 @@ export class PopcornSetup extends MaterialGameSetup<PlayerColor, MaterialType, L
             .map((_) => ({
               id: guestPawn,
               location: {
-                type: LocationType.PlayerGuestPawnsUnderBlothBagSpot,
+                type: LocationType.PlayerGuestPawnsUnderClothBagSpot,
                 player: player
               }
             }))
@@ -230,7 +230,7 @@ export class PopcornSetup extends MaterialGameSetup<PlayerColor, MaterialType, L
       const firstMovieColor = movieCardCharacteristics[movieId].getColor()
       const guestPawnColor = this.getGuestPawnColorFromMovieColor(firstMovieColor)
       this.material(MaterialType.GuestPawns).location(LocationType.GuestPawnReserveSpot).id<GuestPawn>(guestPawnColor).moveItem({
-        type: LocationType.PlayerGuestPawnsUnderBlothBagSpot,
+        type: LocationType.PlayerGuestPawnsUnderClothBagSpot,
         player: player
       })
       this.material(MaterialType.MoneyTokens)
