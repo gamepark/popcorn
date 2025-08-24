608f470e266164b2fecc327e7dc0c792a88515d8
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2025-08-24 22:31:57 +0200

    Feat: added drag&drop for buying theater tiles

--- app/src/locators/TheaterTileOnTopCinemaBoardLocator.ts
+++ app/src/locators/TheaterTileOnTopCinemaBoardLocator.ts
@@ -1,20 +1,40 @@
+import { isBuyTheaterTileCustomMove } from '@gamepark/popcorn/material/CustomMoveType'
 import { LocationType } from '@gamepark/popcorn/material/LocationType'
 import { MaterialType } from '@gamepark/popcorn/material/MaterialType'
 import { PlayerColor } from '@gamepark/popcorn/PlayerColor'
-import { ListLocator, MaterialContext } from '@gamepark/react-game'
-import { Location, MaterialItem } from '@gamepark/rules-api'
+import { DropAreaDescription, ListLocator, MaterialContext } from '@gamepark/react-game'
+import { Location, MaterialItem, MaterialMove } from '@gamepark/rules-api'
+import { theaterTileDescription } from '../material/TheaterTileDescription'
 import { topCinemaBoardDescription } from '../material/TopCinemaBoardDescription'
 
 class TheaterTileOnTopCinemaBoardLocator extends ListLocator<PlayerColor, MaterialType, LocationType> {
   parentItemType = MaterialType.TopCinemaBoard
   coordinates = { x: -6.9, y: -1.15 }
   gap = { x: 7.7, z: 0 }
+  locationDescription = new TheaterTileOnTopCinemaBoardLocatorDescription()
 
   public getParentItem(
     location: Location<PlayerColor, LocationType>,
     context: MaterialContext<PlayerColor, MaterialType, LocationType>
   ): MaterialItem<PlayerColor, LocationType> | undefined {
     return topCinemaBoardDescription.getStaticItems(context).find((topBoardItem) => topBoardItem.location.player === location.player)
+  }
+}
+
+class TheaterTileOnTopCinemaBoardLocatorDescription extends DropAreaDescription<PlayerColor, MaterialType, LocationType> {
+  constructor() {
+    super(theaterTileDescription)
+  }
+
+  public isMoveToLocation(
+    move: MaterialMove<PlayerColor, MaterialType, LocationType>,
+    location: Location<PlayerColor, LocationType>,
+    context: MaterialContext<PlayerColor, MaterialType, LocationType>
+  ): boolean {
+    if (isBuyTheaterTileCustomMove(move)) {
+      return location.player === move.data.player && location.x === move.data.destinationSpot
+    }
+    return super.isMoveToLocation(move, location, context)
   }
 }
 
