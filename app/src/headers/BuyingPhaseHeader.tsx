6ac711a86910ad07ea39904ab951903167a39b9e
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2026-03-06 12:03:26 +0100

    Fix: fixed missing pass button on buying phase heaser
    Signed-off-by: Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>

--- app/src/headers/BuyingPhaseHeader.tsx
+++ app/src/headers/BuyingPhaseHeader.tsx
@@ -1,6 +1,9 @@
+import { LocationType } from '@gamepark/popcorn/material/LocationType.ts'
+import { MaterialType } from '@gamepark/popcorn/material/MaterialType.ts'
 import { PlayerColor } from '@gamepark/popcorn/PlayerColor'
 import { PopcornRules } from '@gamepark/popcorn/PopcornRules'
-import { usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { PlayMoveButton, useLegalMoves, usePlayerId, usePlayerName, useRules } from '@gamepark/react-game'
+import { isStartPlayerTurn, isStartSimultaneousRule, MaterialMove } from '@gamepark/rules-api'
 import { FC } from 'react'
 
 // const PASS_MOVE_TIME = 30
@@ -11,8 +14,18 @@ export const BuyingPhaseHeader: FC = () => {
   const rules = useRules<PopcornRules>()
   const activePlayer = rules?.getActivePlayer()
   const playerName = usePlayerName(activePlayer)
+  const moves = useLegalMoves<MaterialMove<PlayerColor, MaterialType, LocationType>>()
+  const passMove = moves.find(isStartPlayerTurn) ?? moves.find(isStartSimultaneousRule)
+  const isPassOnlyMove = moves.length === 1 && passMove !== undefined
   if (me === activePlayer) {
-    return <>You can buy a movie, a theater, activate an advertising token or PASS</>
+    return (
+      <>
+        You can buy a movie, a theater, activate an advertising token or{' '}
+        <PlayMoveButton move={passMove} {...(isPassOnlyMove ? { auto: 10 } : {})}>
+          PASS
+        </PlayMoveButton>
+      </>
+    )
   }
   return <>{playerName} can buy a movie, a theater, activate an advertising token or pass</>
 }
