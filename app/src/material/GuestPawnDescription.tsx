736337338fea37075e96cb3bc2577a54290a47aa
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2026-03-15 21:20:10 +0100

    Feat: added item buttons for guest pawn during final round

--- app/src/material/GuestPawnDescription.tsx
+++ app/src/material/GuestPawnDescription.tsx
@@ -10,8 +10,9 @@ import { Memory } from '@gamepark/popcorn/Memory'
 import { PlayerColor } from '@gamepark/popcorn/PlayerColor'
 import { RuleId } from '@gamepark/popcorn/rules/RuleId'
 import { ItemContext, ItemMenuButton, TokenDescription } from '@gamepark/react-game'
-import { isSelectItemType, MaterialItem, MaterialMove } from '@gamepark/rules-api'
+import { isMoveItemType, isSelectItemType, MaterialItem, MaterialMove } from '@gamepark/rules-api'
 import React from 'react'
+import { Trans } from 'react-i18next'
 import blueGuestPawn from '../images/GuestPawns/BlueGuestPawn.png'
 import greenGuestPawn from '../images/GuestPawns/GreenGuestPawn.png'
 import redGuestPawn from '../images/GuestPawns/RedGuestPawn.png'
@@ -44,11 +45,42 @@ class GuestPawnDescription extends TokenDescription<PlayerColor, MaterialType, L
   public getItemMenu(
     item: MaterialItem<PlayerColor, LocationType>,
     context: ItemContext<PlayerColor, MaterialType, LocationType>,
-    _legalMoves: MaterialMove<PlayerColor, MaterialType, LocationType>[]
+    legalMoves: MaterialMove<PlayerColor, MaterialType, LocationType>[]
   ): React.ReactNode {
     if (context.player !== undefined) {
+      const pendingActions = context.rules.remind<Actions[]>(Memory.PendingActions, context.player)
+      const nextPendingAction = pendingActions[0]
+      if (
+        nextPendingAction !== undefined &&
+        nextPendingAction.type === ActionType.PickReserveOrExitZoneGuest &&
+        nextPendingAction.guest === GuestPawn.White &&
+        item.id === GuestPawn.White &&
+        (item.location.type === LocationType.GuestPawnReserveSpot ||
+          (item.location.type === LocationType.GuestPawnExitZoneSpotOnTopPlayerCinemaBoard && item.location.player === context.player))
+      ) {
+        const itemIndex = context.index
+        const moveForItem = legalMoves.filter(isMoveItemType(MaterialType.GuestPawns)).filter((move) => move.itemIndex === itemIndex)
+        if (moveForItem.length > 0) {
+          const playerNumbers = moveForItem.length
+          return (
+            <>
+              {moveForItem.map((move, moveIndex) => (
+                <ItemMenuButton
+                  key={`guestPawn-${moveIndex}-bagMove`}
+                  move={move}
+                  x={1.25}
+                  y={(playerNumbers - 1 - 2 * moveIndex) * 0.75 * this.height}
+                  label={<Trans key={`player.${move.location.player}`} defaults={`Send to Player ${move.location.player}'s bag`} />}
+                  labelPosition="right"
+                >
+                  <FontAwesomeIcon icon={faHandPointer} size="xs" />
+                </ItemMenuButton>
+              ))}
+            </>
+          )
+        }
+      }
       if (context.rules.game.rule?.id === RuleId.ShowingsPhaseRule && context.rules.game.players.includes(context.player)) {
-        const pendingActions = context.rules.remind<Actions[]>(Memory.PendingActions, context.player)
         if (pendingActions.length > 0) {
           const pendingAction = pendingActions[0]
           if (pendingAction.type === ActionType.ChooseSeatAction && pendingAction.guestIndex === context.index) {
@@ -58,34 +90,36 @@ class GuestPawnDescription extends TokenDescription<PlayerColor, MaterialType, L
               .parent(item.location.parent)
               .id(item.id)
               .getIndex()
-            const selectCurrentGuestMoves = _legalMoves
+            const selectCurrentGuestMoves = legalMoves
               .filter(isSelectItemType<MaterialType>(MaterialType.GuestPawns))
               .filter((move) => move.itemIndex === currentItemIndex)
-            const passCurrentGuestMoves = _legalMoves.filter(isPassCurrentActionCustomMove)
-            return passCurrentGuestMoves.length > 0 && selectCurrentGuestMoves.length > 0 ? (
-              <>
-                {selectCurrentGuestMoves.map((move) => (
-                  <ItemMenuButton key={`guestPawn-${move.itemIndex}-selectMove`} move={move} x={-1.25} y={this.height / 2}>
-                    <FontAwesomeIcon icon={faHandPointer} size="xs" />
-                  </ItemMenuButton>
-                ))}
-                {passCurrentGuestMoves.map((move) => (
-                  <ItemMenuButton key={`guestPawn-${currentItemIndex}-passMove`} move={move} x={1.25} y={this.height / 2}>
-                    <FontAwesomeIcon icon={faBan} size="xs" />
-                  </ItemMenuButton>
-                ))}
-                {this.getHelpButton(item, context, {
-                  x: 0,
-                  y: this.height / 2 + 2,
-                  label: ''
-                })}
-              </>
-            ) : undefined
+            const passCurrentGuestMoves = legalMoves.filter(isPassCurrentActionCustomMove)
+            if (passCurrentGuestMoves.length > 0 && selectCurrentGuestMoves.length > 0) {
+              return (
+                <>
+                  {selectCurrentGuestMoves.map((move) => (
+                    <ItemMenuButton key={`guestPawn-${move.itemIndex}-selectMove`} move={move} x={-1.25} y={this.height / 2}>
+                      <FontAwesomeIcon icon={faHandPointer} size="xs" />
+                    </ItemMenuButton>
+                  ))}
+                  {passCurrentGuestMoves.map((move) => (
+                    <ItemMenuButton key={`guestPawn-${currentItemIndex}-passMove`} move={move} x={1.25} y={this.height / 2}>
+                      <FontAwesomeIcon icon={faBan} size="xs" />
+                    </ItemMenuButton>
+                  ))}
+                  {this.getHelpButton(item, context, {
+                    x: 0,
+                    y: this.height / 2 + 2,
+                    label: ''
+                  })}
+                </>
+              )
+            }
           }
         }
       }
     }
-    return super.getItemMenu(item, context, _legalMoves)
+    return super.getItemMenu(item, context, legalMoves)
   }
 }
 
