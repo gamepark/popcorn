9a7a43c39ed2207f279c76004376fb5f48c0a71f
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2026-03-16 12:50:26 +0100

    Fix: fixed place guest seat or movie action replacing non activated guests

---
+++ rules/src/rules/actions/utils/getMovieColorFromSpot.ts
@@ -0,0 +1,22 @@
+import { MaterialRulesPart } from '@gamepark/rules-api'
+import { LocationType } from '../../../material/LocationType'
+import { MaterialType } from '../../../material/MaterialType'
+import { movieCardCharacteristics, MovieColor } from '../../../material/MovieCard'
+import { BuyableTheaterTileId } from '../../../material/TheaterTile'
+import { PlayerColor } from '../../../PlayerColor'
+import { RuleId } from '../../RuleId'
+
+export const getMovieColorFromSpot = (
+  rule: MaterialRulesPart<PlayerColor, MaterialType, LocationType, RuleId>,
+  player: PlayerColor,
+  movieSpot: number
+): MovieColor => {
+  const movieCard = rule
+    .material(MaterialType.MovieCards)
+    .player(player)
+    .location(LocationType.MovieCardSpotOnBottomPlayerCinemaBoard)
+    .location((l) => l.x === movieSpot)
+    .getItems<Required<BuyableTheaterTileId>>()[0]
+  const movieCharacteristics = movieCardCharacteristics[movieCard.id.front]
+  return movieCharacteristics.color
+}
