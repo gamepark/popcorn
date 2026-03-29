1454a5df19a033c442cdf3e14efa35b205cacabf
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2026-03-29 23:33:15 +0200

    Feat: added help dialogs for cloth bag

--- app/src/material/ClothBagDescription.ts
+++ app/src/material/ClothBagDescription.ts
@@ -5,12 +5,14 @@ import { RuleId } from '@gamepark/popcorn/rules/RuleId'
 import { FlatMaterialDescription, MaterialContext } from '@gamepark/react-game'
 import { MaterialItem } from '@gamepark/rules-api'
 import clothBag from '../images/Bag.png'
+import { ClothBagHelp } from './help/ClothBagHelp'
 
 class ClothBagDescription extends FlatMaterialDescription<PlayerColor, MaterialType, LocationType, undefined, RuleId, PlayerColor> {
   image = clothBag
   width = 7.6
   height = 6.7
   transparency = true
+  help = ClothBagHelp
 
   public getStaticItems(
     _context: MaterialContext<PlayerColor, MaterialType, LocationType, RuleId, PlayerColor>
