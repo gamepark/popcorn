2cebf9545474fbc98ce51993e44463d4f0750f96
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2026-04-03 16:14:40 +0200

    Fix: fixed theater tile description with wrong action

--- rules/src/material/TheaterTiles/TwoSeatBlue2PopcornMovieActionCharacteristics.ts
+++ rules/src/material/TheaterTiles/TwoSeatBlue2PopcornMovieActionCharacteristics.ts
@@ -2,7 +2,7 @@ import { SeatAction, SeatColor, SeatsNumber } from '../TheaterTile'
 import { getTheaterSeatAction, getTheaterSeatColor, TheaterTileCharacteristics } from './TheaterTileCharacteristics'
 
 export class TwoSeatBlue2PopcornMovieActionCharacteristic implements TheaterTileCharacteristics {
-  private readonly seatActions: SeatAction[] = [SeatAction.Get2Money, SeatAction.MovieAction]
+  private readonly seatActions: SeatAction[] = [SeatAction.Get2Popcorn, SeatAction.MovieAction]
   private readonly seatColors: SeatColor[] = [SeatColor.Blue, SeatColor.Grey]
 
   public getPrice(): number {
