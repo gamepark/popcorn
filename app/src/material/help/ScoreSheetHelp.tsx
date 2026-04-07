e101bfdf31b7ef459278977071855cee7f468cdb
Thibault HUTTIN-PASSERON <87776406+thuttinpasseron@users.noreply.github.com>
2026-04-07 22:13:20 +0200

    Fix: fixed score sheet help, total display

--- app/src/material/help/ScoreSheetHelp.tsx
+++ app/src/material/help/ScoreSheetHelp.tsx
@@ -9,9 +9,15 @@ import awardSymbol from '../../images/Symbols/MovieActionDrawAwardCard.png'
 import totalSymbol from '../../images/Symbols/ScoringTotal.png'
 import { FilmStrip } from '../../theme/filmStrip'
 import {
-  headerDarkCss, headerTitleCss, helpBodyCss,
-  scoreRowCss, scoreIconCss, scoreCategoryCss,
-  tearLineCss, totalBarCss, totalLabelCss, totalValueCss
+  headerDarkCss,
+  headerTitleCss,
+  helpBodyCss,
+  scoreRowCss,
+  scoreIconCss,
+  scoreCategoryCss,
+  tearLineCss,
+  totalBarCss,
+  totalLabelCss
 } from '../../theme/helpStyles'
 
 export const ScoreSheetHelp: FC = () => {
@@ -19,53 +25,53 @@ export const ScoreSheetHelp: FC = () => {
     <>
       <div css={headerDarkCss}>
         <div css={headerTitleCss} style={{ fontSize: '1.25em' }}>
-          <Trans i18nKey="help.material.scoreSheet.title" defaults="Score Sheet"/>
+          <Trans i18nKey="help.material.scoreSheet.title" defaults="Score Sheet" />
         </div>
       </div>
-      <FilmStrip/>
+      <FilmStrip />
       <div css={helpBodyCss}>
         <div>
           <div css={scoreRowCss}>
             <div css={[scoreIconCss, nameBgCss]}>
-              <Picture src={nameSymbol} css={iconPicCss}/>
+              <Picture src={nameSymbol} css={iconPicCss} />
             </div>
             <div css={scoreCategoryCss}>
-              <Trans i18nKey="help.material.scoreSheet.description.name" components={{ icon: <></> }}/>
+              <Trans i18nKey="help.material.scoreSheet.description.name" components={{ icon: <></> }} />
             </div>
           </div>
           <div css={scoreRowCss}>
             <div css={[scoreIconCss, popcornBgCss]}>
-              <Picture src={popcornSymbol} css={iconPicCss}/>
+              <Picture src={popcornSymbol} css={iconPicCss} />
             </div>
             <div css={scoreCategoryCss}>
-              <Trans i18nKey="help.material.scoreSheet.description.popcornDuringGame" components={{ icon: <></> }}/>
+              <Trans i18nKey="help.material.scoreSheet.description.popcornDuringGame" components={{ icon: <></> }} />
             </div>
           </div>
           <div css={scoreRowCss}>
             <div css={[scoreIconCss, moneyBgCss]}>
-              <Picture src={moneySymbol} css={iconPicCss}/>
+              <Picture src={moneySymbol} css={iconPicCss} />
             </div>
             <div css={scoreCategoryCss}>
-              <Trans i18nKey="help.material.scoreSheet.description.popcornMoney" components={{ icon: <></> }}/>
+              <Trans i18nKey="help.material.scoreSheet.description.popcornMoney" components={{ icon: <></> }} />
             </div>
           </div>
           <div css={scoreRowCss}>
             <div css={[scoreIconCss, awardBgCss]}>
-              <Picture src={awardSymbol} css={iconPicCss}/>
+              <Picture src={awardSymbol} css={iconPicCss} />
             </div>
             <div css={scoreCategoryCss}>
-              <Trans i18nKey="help.material.scoreSheet.description.popcornAwardCards" components={{ icon: <></> }}/>
+              <Trans i18nKey="help.material.scoreSheet.description.popcornAwardCards" components={{ icon: <></> }} />
             </div>
           </div>
         </div>
 
-        <div css={tearLineCss}/>
+        <div css={tearLineCss} />
 
         <div css={totalBarCss}>
+          <Picture src={totalSymbol} css={totalIconCss} />
           <div css={totalLabelCss}>
-            <Trans i18nKey="help.material.scoreSheet.description.total" components={{ icon: <></> }} defaults="Total"/>
+            <Trans i18nKey="help.material.scoreSheet.description.total" components={{ icon: <></> }} defaults="Total" />
           </div>
-          <Picture src={totalSymbol} css={totalIconCss}/>
         </div>
       </div>
     </>
@@ -80,9 +86,20 @@ const iconPicCss = css`
 const totalIconCss = css`
   max-width: 1.5em;
   max-height: 1.5em;
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 0.25em;
 `
 
-const nameBgCss = css`background: #E3F2FD;`
-const popcornBgCss = css`background: #FFF3E0;`
-const moneyBgCss = css`background: #E8F5E9;`
-const awardBgCss = css`background: #FFF8E1;`
+const nameBgCss = css`
+  background: #e3f2fd;
+`
+const popcornBgCss = css`
+  background: #fff3e0;
+`
+const moneyBgCss = css`
+  background: #e8f5e9;
+`
+const awardBgCss = css`
+  background: #fff8e1;
+`
